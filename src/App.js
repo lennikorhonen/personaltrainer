8e5bbd80775c677b9ff0df3a269e2b42ea955072
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-04-20 09:47:32 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AppBar from './components/AppBar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <AppBar />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppBar.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { Divider, Drawer, List, ListItem, ListItemText, ListItemIcon, IconButton,
+   AppBar, Toolbar, Typography} from '@material-ui/core';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Customerlist from './Customerlist';
+import Traininglist from './Traininglist';
+
+export default function Bars() {
+
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+      }
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+      }
+
+    return(
+        <div>
+          <AppBar position="static">
+          <Toolbar>
+            <IconButton onClick={handleDrawerOpen} 
+              edge="start" 
+              className={Bars.menuButton} 
+              color="inherit" 
+              aria-label="menu">
+            <MenuIcon />
+            </IconButton>
+          <Typography variant="h6"
+            color='inherit'>
+            Personal trainer
+          </Typography>
+        </Toolbar>
+        </AppBar>
+        <BrowserRouter>
+        <Drawer
+        className={Bars.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes= {{
+          paper: Bars.drawerPaper,
+        }}
+      >
+        <div className={Bars.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button component={Link} to='/' onClick={Customerlist} >
+            <ListItemIcon><AccountBoxIcon /></ListItemIcon>
+            <ListItemText primary='Customers' />
+          </ListItem>
+          <ListItem button component={Link} to='/trainings' onClick={Traininglist} >
+            <ListItemIcon><DirectionsRunIcon /></ListItemIcon>
+            <ListItemText primary='Trainings' />
+          </ListItem>
+        </List>
+      </Drawer>
+      <Switch>
+        <Route exact path='/' component={Customerlist}/>
+        <Route path='/trainings' component={Traininglist} />
+      </Switch>
+      </BrowserRouter>
+        </div>
+    );
+}
