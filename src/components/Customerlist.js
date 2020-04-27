ca89dcf9f0abeda08b071565e5ff252d63f0de14
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-04-27 09:55:21 +0300

    CRUD

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -6,7 +6,6 @@ import Check from '@material-ui/icons/Check';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import ChevronRight from '@material-ui/icons/ChevronRight';
 import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
 import Edit from '@material-ui/icons/Edit';
 import FilterList from '@material-ui/icons/FilterList';
 import FirstPage from '@material-ui/icons/FirstPage';
@@ -15,9 +14,13 @@ import Remove from '@material-ui/icons/Remove';
 import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
+import Snackbar from '@material-ui/core/Snackbar';
+import Delete from '@material-ui/icons/Delete';
 
 export default function Customerlist() {
     const [customers, setCustomers] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
         getCustomers();
@@ -30,12 +33,52 @@ export default function Customerlist() {
         .catch(err => console.error(err))
     };
 
-    
+    const deleteCustomer = (link) => {
+        if (window.confirm('Are you sure?')){
+        fetch(link, {method: 'DELETE'})
+        .then(_ => getCustomers())
+        .then(_ => {
+            setMsg('Customer deleted')
+            setOpen(true)
+        })
+        .catch(err => console.error(err))
+        }
+    }
+
+    const addCustomer = (customer) => {
+        fetch('https://customerrest.herokuapp.com/api/customers',
+        {
+            method: 'POST',
+            header: 'Content-Type: application/json',
+            body: JSON.stringify(customer)
+        })
+        .then(_ => getCustomers())
+        .then(_ => {
+            setMsg('New customer added');
+            setOpen(true);
+        })
+        .catch(err => console.error())
+    }
+
+    const updateCustomer = (link, customer) => {
+        fetch(link, {
+            method: 'PUT',
+            header: 'Content-Type: application/json',
+            body: JSON.stringify(customer)
+        })
+        .then(_=> getCustomers())
+        .then(_=> {
+            setMsg('Customer edited')
+            setOpen(true)
+        })
+        .catch(err => console.error(err))
+    }
+
     const tableIcons = {
         Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
         Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
         Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <Delete {...props} ref={ref} />),
         DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
         Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
         Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
@@ -51,44 +94,90 @@ export default function Customerlist() {
         ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
     };
 
-    const columns = [
-        {
-            title: 'Firstname',
-            field: 'firstname'
-        },
-        {
-            title: 'Lastname',
-            field: 'lastname'
-        },
-        {
-            title: 'Email',
-            field: 'email'
-        },
-        {
-            title: 'Phone',
-            field: 'phone'
-        },
-        {
-            title: 'Address',
-            field: 'streetaddress'
-        },
-        {
-            title: 'Postcode',
-            field: 'postcode'
-        },
-        {
-            title: 'City',
-            field: 'city'
-        }
-    ]
+    const [state, setState] = useState({
+        columns: [
+            {
+                title: 'Firstname',
+                field: 'firstname'
+            },
+            {
+                title: 'Lastname',
+                field: 'lastname'
+            },
+            {
+                title: 'Email',
+                field: 'email'
+            },
+            {
+                title: 'Phone',
+                field: 'phone'
+            },
+            {
+                title: 'Address',
+                field: 'streetaddress'
+            },
+            {
+                title: 'Postcode',
+                field: 'postcode'
+            },
+            {
+                title: 'City',
+                field: 'city'
+            }
+        ]
+    })
 
+    const handleClose = () => {
+        setOpen(false);
+    }
+    
     return(
     <div style={{ maxWidth: "100%", margin: 15 }}>
         <MaterialTable
             icons={tableIcons}
             title="Customers"
-            columns={columns}
-            data={customers} />
+            columns={state.columns}
+            data={customers}
+            actions={[
+                {
+                    icon: () => <Delete></Delete>,
+                    tooltip: 'Delete user',
+                    onClick: (event, row) => {
+                        deleteCustomer(row.links[0].href)
+                    } 
+                },
+                {
+                    icon: () => <Edit></Edit>,
+                    tooltip: 'Edit cutomer',
+                    onClick: (event, row) => {
+                        updateCustomer(row)
+                    }
+                }
+            ]}
+            editable={{
+                onRowAdd: newCustomer =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            {
+                                const data = customers;
+                                data.push(newCustomer);
+                                setState({ data }, () => resolve());
+                            }
+                            resolve()
+                        }, 1000)
+                    })
+            }}
+            />
+        <Snackbar
+            open={open}
+            autoHideDuration={3000}
+            message={msg}
+            onClose={handleClose}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+        />
     </div>
     );
 }
