0e813581d1134c84fd596f9d789cf76014e778b9
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-05-07 12:19:19 +0300

    Delete training works

--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -1,37 +1,18 @@
 import React, { useState, useEffect, forwardRef } from 'react';
 import MaterialTable from 'material-table';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import { AddBox, ArrowDownward, Check, ChevronLeft, ChevronRight, Clear, Edit, FilterList,
+    FirstPage, LastPage, Remove, SaveAlt, Search, ViewColumn, Delete } from '@material-ui/icons';
 import moment from 'moment';
-import Delete from '@material-ui/icons/Delete';
 import Snackbar from '@material-ui/core/Snackbar';
 
 export default function Traininglist() {
     const [trainings, setTrainings] = useState([]);
     const [open, setOpen] = useState(false);
     const [msg, setMsg] = useState('');
-    const [deletable, setDeletable] = useState([]);
 
     useEffect(() => {
         getTrainings();
     },[])
-
-    useEffect(() => {
-        getDelete();
-    })
 
     const getTrainings = () => {
         fetch('https://customerrest.herokuapp.com/gettrainings')
@@ -40,18 +21,11 @@ export default function Traininglist() {
         .catch(err => console.error(err))
     }
 
-    const getDelete = () => {
-        fetch('https://customerrest.herokuapp.com/api/trainings')
-        .then(response => response.json())
-        .then(data => setDeletable(data.content))
-        .catch(err => console.error(err))
-    }
-
     const deleteTrainings = (link) => {
         if(window.confirm('Are you sure'))
-        console.log(link)
-        fetch('https://customerrest.herokuapp.com/api/trainings', {method: 'DELETE'})
-        .then(_ => getDelete())
+        console.log(link.id)
+        fetch('https://customerrest.herokuapp.com/api/trainings/' + link.id, {method: 'DELETE'})
+        .then(_ => getTrainings())
         .then(_ => {
             setMsg('Training deleted')
             setOpen(true)
@@ -63,7 +37,7 @@ export default function Traininglist() {
         Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
         Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
         Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <Delete {...props} ref={ref} />),
         DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
         Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
         Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
@@ -115,7 +89,7 @@ export default function Traininglist() {
                     icon: () => <Delete></Delete>,
                     tooltip: 'Delete training',
                     onClick: (event, row) => {
-                        deleteTrainings(row.links[0].href)
+                        deleteTrainings(row)
                     }
                 }
             ]} />
